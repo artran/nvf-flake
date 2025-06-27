5c2fde407631dff2b97b06193a86af2385dc075a
Ray Tran <rt@elixirsoftware.co.uk>
2025-06-27 16:43:03 +0100

    Make the flake work on multiple systems

--- flake.nix
+++ flake.nix
@@ -2,18 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
-    # flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.aarch64-darwin = {
-      # Set the default package to the wrapped instance of Neovim.
-      # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
-      default =
+  outputs = {nixpkgs, flake-utils, ...} @ inputs:
+    flake-utils.lib.eachSystem [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ]
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default =
         (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          inherit pkgs;
           modules = [
             {
               config.vim = {
@@ -228,6 +231,5 @@
           ];
         })
         .neovim;
-    };
-  };
+    });
 }
